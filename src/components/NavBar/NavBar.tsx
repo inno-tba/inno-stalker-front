9893adbe3c858c64ba30c83aa4792b2768397dc6
k05m0navt <gubaydullin.dan@gmail.com>
2022-04-16 01:09:22 +0300

    Fix menu pop up

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -108,7 +108,7 @@ const NavBar = () => {
                                         leaveFrom='transform opacity-100 scale-100'
                                         leaveTo='transform opacity-0 scale-95'
                                     >
-                                        <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                                        <Menu.Items className='z-50 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
                                             <Menu.Item>
                                                 <p className='block px-4 py-2 text-sm text-gray-700'>
                                                     Your Profile
